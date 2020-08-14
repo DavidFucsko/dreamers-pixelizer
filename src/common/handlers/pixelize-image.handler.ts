6c58217cc6c3fef83c42da2239c12474d73f45d9
David Fucsko <davidzsolt.fucsko@ge.com>
2020-08-14 10:04:30 +0200

    Naive approach with 1000 dominant colors

--- src/common/handlers/pixelize-image.handler.ts
+++ src/common/handlers/pixelize-image.handler.ts
@@ -1,6 +1,10 @@
 import { PixelizeImageRequestParams } from '../../electron/ipc/pixelize-image.request';
 import { PixelizeImageResponse } from '../responses/pixelize-image-response.interface';
 
+const palette = new Map<string, number>();
+let dominantColors: string[] = [];
+let orderedColors: string[] = [];
+
 export async function pixelizeImage(sourceImgParams: PixelizeImageRequestParams): Promise<PixelizeImageResponse> {
 
     const sourceData = sourceImgParams.pixelData;
@@ -8,44 +12,13 @@ export async function pixelizeImage(sourceImgParams: PixelizeImageRequestParams)
 
     console.log(sourceData.length, sourceImgParams.width, sourceImgParams.height);
 
+    const pixelWidth = sourceImgParams.width * 4;
+    const numberOfDoubleLines = ((pixelWidth * sourceImgParams.height)) - (2 * pixelWidth);
+    const doubleLineCount = 2 * pixelWidth;
+
     return new Promise<PixelizeImageResponse>((resolve, reject) => {
-        const pixelWidth = sourceImgParams.width * 4;
-        const numberOfDoubleLines = ((pixelWidth * sourceImgParams.height)) - (2 * pixelWidth);
-        const doubleLineCount = 2 * pixelWidth;
 
         try {
-            for (let i = 0;
-                i <= numberOfDoubleLines;
-                i += doubleLineCount) {
-                for (let j = i; j <= i + pixelWidth; j += 8) {
-                    const lineOffset = j + pixelWidth;
-                    const avgColors = getSuperPixelColorsFromIndex(j, sourceData, lineOffset);
-
-                    // first pixel
-                    pixelData[j] = avgColors.avgRed;
-                    pixelData[j + 1] = avgColors.avgGreen;
-                    pixelData[j + 2] = avgColors.avgBlue;
-                    pixelData[j + 3] = 255;
-
-                    // second pixel
-                    pixelData[j + 4] = avgColors.avgRed;
-                    pixelData[j + 5] = avgColors.avgGreen;
-                    pixelData[j + 6] = avgColors.avgBlue;
-                    pixelData[j + 7] = 255;
-
-                    // third pixel
-                    pixelData[lineOffset] = avgColors.avgRed;
-                    pixelData[lineOffset + 1] = avgColors.avgGreen;
-                    pixelData[lineOffset + 2] = avgColors.avgBlue;
-                    pixelData[lineOffset + 3] = 255;
-
-                    // fourth pixel
-                    pixelData[lineOffset + 4] = avgColors.avgRed;
-                    pixelData[lineOffset + 5] = avgColors.avgGreen;
-                    pixelData[lineOffset + 6] = avgColors.avgBlue;
-                    pixelData[lineOffset + 7] = 255;
-                }
-            }
             // for (let i = 0, n = sourceData.length; i < n; i += 8) {
             //     pixelData[i] = 255 - sourceData[i]; // red
             //     pixelData[i + 1] = 255 - sourceData[i + 1]; // green
@@ -57,6 +30,19 @@ export async function pixelizeImage(sourceImgParams: PixelizeImageRequestParams)
             //     pixelData[i+7] = 255;
             //     // i+3 is alpha (the fourth element)
             // }
+            iterateOverData(
+                extractDominantColors.bind(this, sourceData, pixelWidth),
+                numberOfDoubleLines,
+                doubleLineCount,
+                pixelWidth
+            );
+            createPalette();
+            iterateOverData(
+                colorizePixels.bind(this, pixelData, pixelWidth, sourceData),
+                numberOfDoubleLines,
+                doubleLineCount,
+                pixelWidth
+            );
             resolve({
                 pixelArtImage: {
                     pixels: pixelData,
@@ -68,6 +54,136 @@ export async function pixelizeImage(sourceImgParams: PixelizeImageRequestParams)
             reject(new Error(error));
         }
     });
+}
+
+function isColorLight(r: number, g: number, b: number) {
+    // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+    const hsp = Math.sqrt(
+        0.299 * (r * r) +
+        0.587 * (g * g) +
+        0.114 * (b * b)
+    );
+
+    // Using the HSP value, determine whether the color is light or dark
+    if (hsp > 127.5) {
+        return true;
+    }
+    return false;
+}
+
+function iterateOverData(
+    functionToRun: () => void,
+    numberOfDoubleLines: number,
+    doubleLineCount: number,
+    pixelWidth: number) {
+    for (let i = 0;
+        i <= numberOfDoubleLines;
+        i += doubleLineCount) {
+        for (let j = i; j <= i + pixelWidth; j += 8) {
+            const newFunc = functionToRun.bind(null, j);
+            newFunc();
+        }
+    }
+}
+
+function colorizePixels(
+    pixelData: Uint8ClampedArray,
+    pixelWidth: number,
+    sourceData: Uint8ClampedArray,
+    index?: number) {
+
+    const lineOffset = index + pixelWidth;
+    const avgColors = getSuperPixelColorsFromIndex(index, sourceData, lineOffset);
+
+    const color = findColorFromPalette(avgColors);
+
+    // first pixel
+    pixelData[index] = color.red;
+    pixelData[index + 1] = color.green;
+    pixelData[index + 2] = color.blue;
+    pixelData[index + 3] = 255;
+
+    // second pixel
+    pixelData[index + 4] = color.red;
+    pixelData[index + 5] = color.green;
+    pixelData[index + 6] = color.blue;
+    pixelData[index + 7] = 255;
+
+    // third pixel
+    pixelData[lineOffset] = color.red;
+    pixelData[lineOffset + 1] = color.green;
+    pixelData[lineOffset + 2] = color.blue;
+    pixelData[lineOffset + 3] = 255;
+
+    // fourth pixel
+    pixelData[lineOffset + 4] = color.red;
+    pixelData[lineOffset + 5] = color.green;
+    pixelData[lineOffset + 6] = color.blue;
+    pixelData[lineOffset + 7] = 255;
+}
+
+function extractDominantColors(
+    sourceData: Uint8ClampedArray,
+    pixelWidth: number,
+    index?: number) {
+    const lineOffset = index + pixelWidth;
+    const avgColors = getSuperPixelColorsFromIndex(index, sourceData, lineOffset);
+
+    const hashedColor = ("00" + avgColors.red.toString()).slice(-3) + ("00" + avgColors.green.toString()).slice(-3) + ("00" + avgColors.blue.toString()).slice(-3);
+    const currentWeight = palette.get(hashedColor);
+    if (currentWeight) {
+        palette.set(hashedColor, currentWeight + 1);
+    } else {
+        palette.set(hashedColor, 1);
+    }
+}
+
+function createPalette() {
+    palette[Symbol.iterator] = function* () {
+        yield* [...this.entries()].sort((a, b) => a[1] - b[1]);
+    }
+
+    for (let [key, value] of palette) {
+        orderedColors.push(key);
+    }
+
+    dominantColors = orderedColors.slice(0, orderedColors.length < 1000 ? orderedColors.length : 1000);
+}
+
+function findColorFromPalette(pixelColor: { red: number, green: number, blue: number }) {
+    let closestColorHash = 0;
+    let closestColor = { red: 0, green: 0, blue: 0 };
+    dominantColors.map(colorCode => ({
+        red: parseInt(colorCode.toString().slice(0, 3), 10),
+        green: parseInt(colorCode.toString().slice(3, 6), 10),
+        blue: parseInt(colorCode.toString().slice(6, 9), 10)
+    })).forEach((color, index) => {
+        const colorDiff = colorDifference(
+            pixelColor.red,
+            pixelColor.green,
+            pixelColor.blue,
+            color.red,
+            color.green,
+            color.blue
+        );
+
+        if (index === 0 || colorDiff < closestColorHash) {
+            closestColorHash = colorDiff;
+            closestColor = color;
+        }
+    });
+
+    return closestColor;
+}
+
+function colorDifference(r1: number, g1: number, b1: number, r2: number, g2: number, b2: number) {
+    let sumOfSquares = 0;
+
+    sumOfSquares += Math.pow(r1 - r2, 2);
+    sumOfSquares += Math.pow(g1 - g2, 2);
+    sumOfSquares += Math.pow(b1 - b2, 2);
+
+    return Math.sqrt(sumOfSquares);
 }
 
 function getSuperPixelColorsFromIndex(j: number, pixelData: Uint8ClampedArray, lineOffset: number) {
@@ -111,8 +227,8 @@ function getSuperPixelColorsFromIndex(j: number, pixelData: Uint8ClampedArray, l
         fourthPixel.blue].reduce((a, b) => a + b, 0) / 4;
 
     return {
-        avgRed: parseInt(avgRed.toString(), 10),
-        avgGreen: parseInt(avgGreen.toString(), 10),
-        avgBlue: parseInt(avgBlue.toString(), 10)
+        red: parseInt(avgRed.toString(), 10),
+        green: parseInt(avgGreen.toString(), 10),
+        blue: parseInt(avgBlue.toString(), 10)
     };
 }
